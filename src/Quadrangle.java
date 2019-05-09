b0dcff9db6cb48137bd5a723152e64fb992f65b1
Hugo <blaison.hugo@gmail.com>
2019-05-09 14:21:05 +0200

    Quadrangle

---
+++ src/Quadrangle.java
@@ -0,0 +1,13 @@
+
+public class Quadrangle extends ObjetBasique {
+	
+	private double rayon;
+	
+	public Quadrangle(Point2D p0, Point2D p1, Point2D p2, Point2D p3) {
+		
+		this.addPoint(p0);
+		this.addPoint(p1);
+		this.addPoint(p2);
+		this.addPoint(p3);
+	}
+}
