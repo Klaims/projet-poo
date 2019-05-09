26a9154f116d8e4c191a009fd046771b83107f4b
HamaraH <arno.ramm2@gmail.com>
2019-05-09 14:42:02 +0200

    Rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,7 +1,8 @@
 
-public class Losange extends ObjetBasique implements Informations{
+public class Rectangle extends ObjetGeometrique {
 	
-	public Losange(Point2D p1, Point2D p2, Point2D p3 , Point2D p4) {
+
+	public Rectangle(Point2D p1, Point2D p2, Point2D p3 , Point2D p4) {
 		
 		this.addPoint(p1);
 		this.addPoint(p2);
@@ -13,7 +14,7 @@ public class Losange extends ObjetBasique implements Informations{
 	
 	public String toString() {
 		
-		return    ("Le losange est composé des quatres points :"
+		return    ("Le rectangle est composé des quatres points :"
 				  + "  p1 (" + (this.getPoint(0).getPosX() + "," + (this.getPoint(0).getPosY() + ")"
 				  + " p2 (" +  ( this.getPoint(1)).getPosX()) + " , " +  this.getPoint(1).getPosY() + ")" 
 				  + " p3 (" +  ( this.getPoint(2)).getPosX() + " , " +  this.getPoint(2).getPosY() + ")"  
