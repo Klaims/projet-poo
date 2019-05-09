96d736c44f2be5a9a46b96d5063b5184b979ddfe
Manon5 <manon.touba05@gmail.com>
2019-05-09 13:59:51 +0200

    Update Segment.java

--- src/Segment.java
+++ src/Segment.java
@@ -1,3 +1,13 @@
 public class Segment extends ObjetBasique{
+  
+  public Segment(Point2D p1, Point2D p2){
+    // On ajoute les points à l'ArrayList
+    this.addPoint(p1);
+    this.addPoint(p2);
+    // On calcule le point de référence = milieu du segment
+    double x = (p1.getPosX() + p2.getPosX())/2;
+    double y = (p1.getPosY() + p2.getPosY())/2;
+    this.setRef(new Point2D(x, y);
+  }
 
 }
