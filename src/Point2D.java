223b8c872a4ec152c2512ec835873167368f8113
HamaraH <arno.ramm2@gmail.com>
2019-05-09 13:55:31 +0200

    Poin2D et Vecteur2D

--- src/Point2D.java
+++ src/Point2D.java
@@ -1,5 +1,44 @@
 
 public class Point2D {
 
-	// TODO
+	private double posX;
+	private double posY;
+	
+	public Point2D(double x, double y) {
+		
+		this.posX=x;
+		this.posY=y;
+		
+	}
+
+	public double getPosX() {
+		
+		return posX;
+		
+	}
+
+	public void setPosX(double posX) {
+		
+		this.posX = posX;
+		
+	}
+
+	public double getPosY() {
+		
+		return posY;
+		
+	}
+
+	public void setPosY(double posY) {
+		
+		this.posY = posY;
+		
+	}
+
+	public String toString() {
+		
+		return ("Abscisse : " + posX + " Ordonnée : " + posY);
+		
+	}
+	
 }
