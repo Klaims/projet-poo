8eeddf0a6c7a171adee3dd4ffe3ca971185f0299
Hugo <blaison.hugo@gmail.com>
2019-05-10 21:06:35 +0200

    Affichage du statut

--- src/Graphique/BoutonOutil.java
+++ src/Graphique/BoutonOutil.java
@@ -1,16 +1,26 @@
 package Graphique;
 
 import java.awt.Dimension;
-import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
-public class BoutonOutil extends JButton{
+public class BoutonOutil extends JButton implements ActionListener{
 
 	public BoutonOutil() {
 		
 		// Settings bouton
 		this.setPreferredSize(new Dimension(100,60));
+		this.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		JLabel label = ((JLabel) this.getParent().getParent().getComponent(1));
+		
+		label.setText("Statut : " + ((BoutonOutil) e.getSource()).getText() );
 	}
 }
