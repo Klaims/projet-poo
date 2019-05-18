5bf1d368116cfcb13579bc5b090b4d7cadb1e9dc
Hugo <blaison.hugo@gmail.com>
2019-05-18 17:01:07 +0200

    Constructeur

--- src/ObjetsBasiques/ArcCercle.java
+++ src/ObjetsBasiques/ArcCercle.java
@@ -9,8 +9,9 @@ public class ArcCercle extends Cercle implements Informations,CalculsGeometrique
 	private int angle_deb;
 	private int angle_fin;
 	
-	public ArcCercle(Point2D p, double r, int angle_deb, int angle_fin) {
-
+	public ArcCercle(Point2D centre, double rayon, int angle_deb, int angle_fin) {
+		super(centre, rayon);
+		
 		this.angle_deb = angle_deb;
 		this.angle_fin = angle_fin;
 	}	
