187f85ccf451efc0e880ee33157987c097ebd4bd
Manon <touba8u@IUTM-INF-F36-19.iutmetz.site.univ-lorraine.fr>
2019-06-06 08:37:46 +0200

    gb

---
+++ src/ObjetsComposites/MultiRectangle.java
@@ -0,0 +1,18 @@
+package ObjetsComposites;
+
+import java.util.ArrayList;
+
+import General.ObjetBasique;
+import General.ObjetComposite;
+
+public class MultiRectangle extends ObjetComposite{
+	
+	public MultiRectangle(ArrayList<ObjetBasique> a) {
+		super(a);
+	}
+
+	public String infosObjet() {
+		return "à faire plus tard";
+	}
+
+}
---
+++ src/ObjetsComposites/SuiteEllipse.java
@@ -0,0 +1,20 @@
+package ObjetsComposites;
+
+import java.util.ArrayList;
+
+import General.ObjetBasique;
+import Interfaces.Informations;
+
+public class SuiteEllipse extends Multisegment implements Informations{
+
+	public SuiteEllipse(ArrayList<ObjetBasique> a) {
+		super(a);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String infosObjet() {
+		// TODO Auto-generated method stub
+		return "à faire plus tard";
+	}
+	
+}
